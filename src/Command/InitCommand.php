cb4d473908ea302cd6931782e775ba18723018ab
Nik Samokhvalov <nik@samokhvalov.info>
2016-02-09 10:04:11 +0300

    Init command. Work in progress...

--- src/Command/InitCommand.php
+++ src/Command/InitCommand.php
@@ -6,8 +6,11 @@
 
 namespace Notamedia\ConsoleJedi\Command;
 
+use Symfony\Component\Console\Helper\DialogHelper;
+use Symfony\Component\Console\Helper\QuestionHelper;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\ConfirmationQuestion;
 
 class InitCommand extends Command
 {
@@ -25,6 +28,70 @@ class InitCommand extends Command
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        /**
+         * @var QuestionHelper $question
+         */
+        $questionHelper = $this->getHelper('question');
+                
+        $question = new ConfirmationQuestion(
+            '<question>Create a directories for environment settings?</question> [Y/n]', 
+            true, 
+            '/^(y|j)/i'
+        );
+
+        if ($questionHelper->ask($input, $output, $question)) {
+            $this->createEnvironmentDir($input, $output);
+        }
+
+        $question = new ConfirmationQuestion(
+            '<question>Create file for run Console Jedi application?</question> [Y/n]',
+            true,
+            '/^(y|j)/i'
+        );
+
+        if ($questionHelper->ask($input, $output, $question)) {
+            $this->createApplicationFile($input, $output);
+        }
+    }
+    
+    protected function createEnvironmentDir(InputInterface $input, OutputInterface $output)
+    {
         
+    }
+    
+    protected function createApplicationFile(InputInterface $input, OutputInterface $output)
+    {
+        /**
+         * @var DialogHelper $dialogHelper
+         */
+        $dialogHelper = $this->getHelper('dialog');
+        
+        $path = getcwd() . '/jedi';
+
+        $pathAnswer = $dialogHelper->select(
+            $output,
+            'Create application file in ' . $path,
+            ['Yes', 'Another path', 'Cancel'],
+            0
+        );
+
+        if ($pathAnswer === '1' || $pathAnswer == 0)
+        {
+            $path = $dialogHelper->askAndValidate(
+                $output,
+                'Enter the path to file: ',
+                function ($answer) {
+                    if (!$answer) {
+                        throw new \RuntimeException(
+                            'Path "' . $answer . '" is invalid"'
+                        );
+                    }
+
+                    return $answer;
+                }
+            );
+            
+            echo 'CREATED';
+        }
     }
 }
