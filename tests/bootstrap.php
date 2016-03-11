d86f907af2089ecbf889bece9d66fbd30ba0daa7
Nik Samokhvalov <nik@samokhvalov.info>
2016-03-11 20:23:54 +0300

    PHPUnit. Work in progress...

---
+++ tests/bootstrap.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * For the full copyright and license information, please view the LICENSE.md
+ * file that was distributed with this source code.
+ */
+
+require_once __DIR__ . '/../../../autoload.php';
+
+$app = new \Notamedia\ConsoleJedi\Console\Application();
+$app->loadConfiguration();
+$app->initializeBitrix();
+
+$loader = new \Notamedia\ConsoleJedi\Loader();
+$loader->loadTests();
