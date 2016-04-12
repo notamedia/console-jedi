a580778e7c90ad8b349f4865f952b6ef4e44b8e3
Marat Shamshutdinov <marat@citrus-soft.ru>
2016-04-12 15:25:08 +0300

    fixed ident

--- src/Search/Command/ReIndexCommand.php
+++ src/Search/Command/ReIndexCommand.php
@@ -22,61 +22,61 @@ use Symfony\Component\Console\Output\OutputInterface;
  */
 class ReIndexCommand extends BitrixCommand
 {
-	const UPDATE_TIME = 5;
+    const UPDATE_TIME = 5;
 
-	/**
-	 * {@inheritdoc}
-	 */
-	protected function configure()
-	{
-		parent::configure();
+    /**
+     * {@inheritdoc}
+     */
+    protected function configure()
+    {
+        parent::configure();
 
-		$this->setName('search:reindex')
-			->setDescription('Rebuild search index')
-			->addOption('full', 'f', InputOption::VALUE_NONE,
-				'Clears existing index (otherwise only changed entries would be indexed)');
-	}
+        $this->setName('search:reindex')
+            ->setDescription('Rebuild search index')
+            ->addOption('full', 'f', InputOption::VALUE_NONE,
+                'Clears existing index (otherwise only changed entries would be indexed)');
+    }
 
-	/**
-	 * {@inheritdoc}
-	 */
-	protected function execute(InputInterface $input, OutputInterface $output)
-	{
-		if (!Loader::includeModule('search'))
-		{
-			throw new BitrixException('Search module is not installed');
-		}
+    /**
+     * {@inheritdoc}
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        if (!Loader::includeModule('search'))
+        {
+            throw new BitrixException('Search module is not installed');
+        }
 
-		$searchResult = array();
+        $searchResult = array();
 
-		$bar = new ProgressBar($output, 0);
-		do
-		{
-			$bar->display();
+        $bar = new ProgressBar($output, 0);
+        do
+        {
+            $bar->display();
 
-			$searchResult = \CSearch::ReIndexAll($input->getOption('full'), static::UPDATE_TIME, $searchResult);
+            $searchResult = \CSearch::ReIndexAll($input->getOption('full'), static::UPDATE_TIME, $searchResult);
 
-			$bar->advance();
-			$bar->clear();
+            $bar->advance();
+            $bar->clear();
 
-			if (is_array($searchResult) && $searchResult['MODULE'] == 'main')
-			{
-				list($site, $path) = explode("|", $searchResult["ID"], 2);
-				$output->writeln("\r       " . $path, OutputInterface::VERBOSITY_VERBOSE);
-			}
-		} while (is_array($searchResult));
+            if (is_array($searchResult) && $searchResult['MODULE'] == 'main')
+            {
+                list(, $path) = explode("|", $searchResult["ID"], 2);
+                $output->writeln("\r       " . $path, OutputInterface::VERBOSITY_VERBOSE);
+            }
+        } while (is_array($searchResult));
 
-		$bar->finish();
-		$bar->clear();
-		$output->write("\r");
+        $bar->finish();
+        $bar->clear();
+        $output->write("\r");
 
-		if (ModuleManager::isModuleInstalled('socialnetwork'))
-		{
-			$output->writeln('<info>The Social Network module needs to be reindexed using the Social Network component in the public section of site.</info>');
-		}
+        if (ModuleManager::isModuleInstalled('socialnetwork'))
+        {
+            $output->writeln('<info>The Social Network module needs to be reindexed using the Social Network component in the public section of site.</info>');
+        }
 
-		$output->writeln(sprintf('<info>Reindexed</info> %d element%s.', $searchResult, $searchResult > 1 ? 's' : ''));
+        $output->writeln(sprintf('<info>Reindexed</info> %d element%s.', $searchResult, $searchResult > 1 ? 's' : ''));
 
-		return 0;
-	}
+        return 0;
+    }
 }
